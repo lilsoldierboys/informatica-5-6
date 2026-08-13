f0dcaa32b7cb4d346d7ea7e4444cec9200da9772
Cesar Franco <cesarantoniofranco14@gmail.com>
2026-08-13 15:08:08 +0000

    story

---
+++ story.py
@@ -0,0 +1,13 @@
+def main():
+    name = input("What is your name? ")
+    color = input("Color: ")
+    adjective = input("Adjective: " )
+    goal = input("Goal: ")
+
+    print("Hello \n", name)
+
+    print("This is your: ")
+
+    print(f"At dawn the sky turned {color}, and the air felt {adjective}. I decided today I will finally {goal}.")
+
+main()
