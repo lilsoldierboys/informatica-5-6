26cbc217903479fc6bb3ede8fd76237f71c2f483
Cesar Franco <cesarantoniofranco14@gmail.com>
2026-09-02 17:59:15 +0000

    bol

--- Week4/roadtrip.py
+++ Week4/roadtrip.py
@@ -6,5 +6,8 @@ answ = ""
 while answ != "yes":
     time.sleep(1.5)
     answ = input("Are we there yet? ").lower().strip()
-
+if answ == "yes":
+    conf = input("Really? " )
+    if conf == "yes":
+        print("yay")
 
