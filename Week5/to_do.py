ba4df0cc40e84f2edb8b02463afed9fdad58ec8e
Cesar Franco <cesarantoniofranco14@gmail.com>
2026-09-17 14:51:10 +0000

    f

---
+++ Week5/to_do.py
@@ -0,0 +1,34 @@
+to_do = []
+
+print(f"tasks to do {len(to_do)}")
+answ = input("Do you want to add a task? ")
+
+
+
+
+
+while True:
+    print(f"tasks to do {len(to_do)}")
+    if answ == "remove":
+
+        #if answ == "complete all":
+             #to_do.clear()
+        else:
+            remove = input("What task do you want to remove? ")
+            to_do.remove(remove)
+            answ = input("Do you want to add, remove a task or exit? ")
+        print(answ)
+    elif answ == "exit":
+        print(to_do)
+        break
+
+
+
+    elif answ == "y" or "add":
+            print(to_do)
+            x = input("Task you want to add: ")
+            to_do.append(x)
+            answ = input("Do you want to add, remove a task or exit? ")
+
+
+
