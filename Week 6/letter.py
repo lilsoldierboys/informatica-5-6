adaeb4aa429b550886d0d95d2ed01c3b5be78ec1
Cesar Franco <cesarantoniofranco14@gmail.com>
2026-09-22 20:03:25 +0000

    ddfsdf

---
+++ Week 6/letter.py
@@ -0,0 +1,22 @@
+names = ["mario", "luigi", "peach", "bowser", "Daisy", "Yoshi", "Toad", "Rosalina"]
+sender = names[2]
+num = 0
+for i in range(7):
+    receiver = names[num]
+    num += 1
+    if receiver == "peach":
+        receiver = names[num + 1]
+    print(f"""
+    +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
+       Dear {receiver},
+
+       You are cordially invited to a ball at
+       Peach's Castle this evening, 7:00 PM.
+
+       Sincerely,
+       {sender}
+    +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
+""")
+
+
+
